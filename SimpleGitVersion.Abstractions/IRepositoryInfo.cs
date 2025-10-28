ea1307d5089badfb4d2b5a9866d7478c3d4b97b8
Olivier Spinelli <olivier.spinelli@signature-code.com>
2025-10-28 13:44:37 +0100

    using file scoped namespace.

--- SimpleGitVersion.Abstractions/IRepositoryInfo.cs
+++ SimpleGitVersion.Abstractions/IRepositoryInfo.cs
@@ -1,19 +1,18 @@
-namespace SimpleGitVersion
+namespace SimpleGitVersion;
+
+/// <summary>
+/// Minimal repository information.
+/// </summary>
+public interface IRepositoryInfo
 {
     /// <summary>
-    /// Minimal repository information.
+    /// Gets the remote url "origin" if found.
     /// </summary>
-    public interface IRepositoryInfo
-    {
-        /// <summary>
-        /// Gets the remote url "origin" if found.
-        /// </summary>
-        public string? RemoteUrl { get; }
+    public string? RemoteUrl { get; }
 
-        /// <summary>
-        /// Gets the working folder (where /.git sub folder is)
-        /// if it has been found and is valid.
-        /// </summary>
-        public string? WorkingDirectory { get; }
-    }
+    /// <summary>
+    /// Gets the working folder (where /.git sub folder is)
+    /// if it has been found and is valid.
+    /// </summary>
+    public string? WorkingDirectory { get; }
 }
